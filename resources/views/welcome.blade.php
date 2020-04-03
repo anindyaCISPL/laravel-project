ddb6d5016428e17274d7177ca79cfa58519da38f
Anindya CISPL <anindya@codeclouds.io>
2020-04-03 16:13:58 +0530

    Update your home page for new feature

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to Laravel 7
                 </div>
 
                 <div class="links">
